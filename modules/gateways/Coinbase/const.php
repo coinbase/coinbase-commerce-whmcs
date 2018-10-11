c193aba679a6925c7b9e019d313d48a5c0529c0d
chalk777 <igor.meleshko@gmail.com>
2018-10-11 18:58:23 +0300

    Added cancel url

--- modules/gateways/Coinbase/const.php
+++ modules/gateways/Coinbase/const.php
@@ -1,5 +1,6 @@
 <?php
-
+defined('METADATA_CLIENT_PARAM') or  define('METADATA_CLIENT_PARAM', 'clientid');
+defined('METADATA_INVOICE_PARAM') or  define('METADATA_INVOICE_PARAM', 'invoiceid');
 defined('METADATA_SOURCE_PARAM') or  define('METADATA_SOURCE_PARAM', 'source');
 defined('METADATA_SOURCE_VALUE') or define('METADATA_SOURCE_VALUE', 'whmcs');
 defined('SIGNATURE_HEADER') or define('SIGNATURE_HEADER', 'x-cc-webhook-signature');
